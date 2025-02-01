9a73692733d9106dfc166962e81513e544582900
Butenkite <174657090+Butenkite@users.noreply.github.com>
2025-02-01 12:32:04 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+let data = [];
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000;
+
+app.get('/', (req, res) =>{
+    res.sendFile(`${import.meta.dirname}/views/home.html`)
+});
+
+
+
+app.post('/confirmation', (req,res) =>{
+    data.push(req.body);
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+})
+
+app.get('/admin', (req, res) =>{
+    res.send(data);
+    console.log(data);
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
