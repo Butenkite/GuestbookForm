705cd4772157cdff6f7e61e1cb60ea08c0c0da44
unknown <174657090+Butenkite@users.noreply.github.com>
2025-02-08 20:57:31 -0800

    Part 2

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-import express from 'express';
+import express from "express";
 
 let data = [];
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
-app.get('/', (req, res) =>{
-    res.sendFile(`${import.meta.dirname}/views/home.html`)
+app.get("/", (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
-
-
-app.post('/confirmation', (req,res) =>{
-    data.push(req.body);
-    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
-})
-
-app.get('/admin', (req, res) =>{
-    res.send(data);
-    console.log(data);
+app.post("/confirmation", (req, res) => {
+  data.push(req.body);
+  res.sendFile(`${import.meta.dirname}/views/confirm.html`);
 });
 
-app.listen(PORT, () =>{
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
+app.get("/admin", (req, res) => {
+  res.send(data);
+  console.log(data);
+});
+
+app.post("/home", (req, res) => {
+  data.push(req.body);
+  res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
